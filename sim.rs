2255f8407045e86e29e4650e849847245dc5d048
Diego Ongaro <ongaro@cs.stanford.edu>
2013-11-12 14:00:54 -0800

    Rewrite pop_ready_messages

--- sim.rs
+++ sim.rs
@@ -1,18 +1,17 @@
 #[feature(globs)];
 extern mod std;
-use std::fmt;
 use basics::*;
 use policies::TimingPolicy;
 use raft::{Message, MessageBody, Server};
 
 struct SimMessage {
     deliver: Time,
-    msg: ~Message,
+    msg: Message,
 }
 
 struct Environment {
     clock: Time,
-    network: ~[~SimMessage],
+    network: ~[SimMessage],
     timing: ~TimingPolicy,
 }
 
@@ -29,9 +28,9 @@ impl Environment {
     }
     pub fn multicast(&mut self, from: ServerID, to: &[ServerID], body: &MessageBody) {
         for peer in to.iter() {
-            let m = ~SimMessage {
+            let m = SimMessage {
                 deliver: self.clock + self.timing.network_latency(from, *peer),
-                msg: ~Message {
+                msg: Message {
                     from: from,
                     to: *peer,
                     body: *body,
@@ -41,9 +40,9 @@ impl Environment {
         }
     }
     pub fn reply(&mut self, request: &Message, response_body: &MessageBody) {
-        let m = ~SimMessage {
+        let m = SimMessage {
             deliver: self.clock + self.timing.network_latency(request.to, request.from),
-            msg: ~Message {
+            msg: Message {
                 from: request.to,
                 to: request.from,
                 body: *response_body,
@@ -51,20 +50,15 @@ impl Environment {
         };
         self.network.push(m);
     }
-    pub fn pop_ready_messages(&mut self) -> ~[~Message] {
-    // TODO: wow this needs a rewrite
-        if !self.network.iter().any(|msg| msg.deliver <= self.clock) {
-            return ~[];
+    pub fn get_ready_messages(&mut self, out: &mut ~[Message]) {
+        let mut i = 0;
+        while i < self.network.len() {
+            if self.network[i].deliver <= self.clock {
+                out.push(self.network.swap_remove(i).msg);
+            } else {
+                i += 1;
+            }
         }
-        let mut all = ~[];
-        std::util::swap(&mut all, &mut self.network);
-        let (ready, notready) = all.partition(|msg| msg.deliver <= self.clock);
-        self.network = notready;
-        let mut ready2 = ~[];
-        for m in ready.move_iter() {
-            ready2.push(m.msg);
-        }
-        return ready2;
     }
 }
 
@@ -95,14 +89,20 @@ pub fn simulate(num_servers: uint, timing_policy: ~TimingPolicy) -> Time {
     let env = &mut Environment::new(timing_policy);
     let mut cluster = Cluster::new(env, num_servers);
     let mut end = None;
+    let mut ready_msgs = ~[]; // declared out here to avoid mallocs
     while end.is_none() {
         env.clock = Time(*env.clock + 1);
         for server in cluster.mut_iter() {
             server.tick(env);
         }
-        for msg in env.pop_ready_messages().move_iter() {
+
+        // deliver new messages
+        env.get_ready_messages(&mut ready_msgs);
+        for msg in ready_msgs.iter() {
             cluster.deliver(env, msg);
         }
+        ready_msgs.truncate(0);
+
         for server in cluster.iter() {
             end = server.stable_leader_start_time();
             if end.is_some() {
