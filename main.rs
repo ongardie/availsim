91d1b0d3e97f2e921f677977eca8a90a5fb18225
Diego Ongaro <ongaro@cs.stanford.edu>
2013-12-08 17:56:47 -0800

    Switch big sort back to merge sort for perf

--- main.rs
+++ main.rs
@@ -339,7 +339,8 @@ struct Sample {
 }
 
 fn sort_samples(samples: &mut ~[Sample]) {
-    extra::sort::quick_sort(*samples, |x,y| x.ticks <= y.ticks);
+    // merge sort seems to greatly outperform quicksort for about 2500 samples
+    *samples = extra::sort::merge_sort(*samples, |x,y| x.ticks <= y.ticks);
 }
 
 fn run_task<T: Send>(mut runs: std::iter::RangeStep<uint>, opts: &sim::SimOpts, exit_port: &Port<T>) -> ~[Sample] {
@@ -355,6 +356,5 @@ fn run_task<T: Send>(mut runs: std::iter::RangeStep<uint>, opts: &sim::SimOpts, 
         };
         samples.push(sample);
     }
-    sort_samples(&mut samples);
     return samples;
 }
